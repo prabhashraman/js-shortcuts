3092123457d34776adb0a42f3fe06d9dab02c948
prabhashr <finelogics.prabhash@gmail.com>
2023-05-17 19:00:59 +0530

    PR

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function GetCurrentDate(){
-	return date;
+function GetCurrentDateDDMMYYYY(){
+	const today = new Date();
+	const yyyy = today.getFullYear();
+let mm = today.getMonth() + 1;
+	let dd = today.getDate();
+	if (dd < 10) dd = '0' + dd; if (mm < 10) mm = '0' + mm; const formattedToday = dd + '-' + mm + '-' + yyyy;
+	return formattedToday;
+}
+function GetCurrentDateTimeDDMMYYYY(){
+	const today = new Date();
+const yyyy = today.getFullYear();
+let mm = today.getMonth() + 1;
+	let dd = today.getDate();
+	if (dd < 10) dd = '0' + dd; if (mm < 10) mm = '0' + mm; const formattedToday = dd + '-' + mm + '-' + yyyy;
+	return formattedToday;
+}
+function GetCurrentDateYYYYMMDD(){
+	const formattedToday = new Date(new Date().getTime() - (new Date().getTimezoneOffset() * 60000)).toISOString().substr(0, 10).replace('T', ' ')
+	return formattedToday;
+}
+function GetCurrentDateTimeYYYYMMDD(){
+	const formattedToday = new Date(new Date().getTime() - (new Date().getTimezoneOffset() * 60000)).toISOString().substr(0, 19).replace('T', ' ')
+	return formattedToday;
 }
 function GetFormattedDate(date){
 	return date;
